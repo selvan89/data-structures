7364bcaa8c4cb5c97d67769bf5c73a786b1de9f2
selvan89 <selvanprograms@gmail.com>
2014-12-10 19:49:13 -0800

    Finish prototypal pattern Queue and Stack

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,33 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
-  var someInstance = Object.create(queueMethods.things);
+  var someInstance = Object.create(queueMethods);
   someInstance.storage = {};
   someInstance.counter = 0;
   someInstance.nextUp = 0;
   someInstance.lengthSize = 0;
 
-
-
   return someInstance;
 };
 
-var queueMethods.things = {};
+var queueMethods = {};
 
-queueMethods.things.enqueue = function(value){
+queueMethods.enqueue = function(value){
 
   this.storage[this.counter] = value;
   this.counter++;
   this.lengthSize++;
-}
-queueMethods.things.dequeue = function(){
+};
+
+queueMethods.dequeue = function(){
   var value = this.storage[this.nextUp];
   delete this.storage[this.nextUp];
   this.nextUp++;
   this.lengthSize--;
   return value;
 
-}
-queueMethods.things.size = function(){
+};
+
+queueMethods.size = function(){
   return this.lengthSize < 0 ? 0 : this.lengthSize;
-}
+};
+
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  someInstance.counter = 0;
+  someInstance.storage = {};
+
+  return someInstance;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value){
+  this.storage[this.counter] = value;
+  this.counter++;
 
+}
+
+stackMethods.pop = function(){
+  this.counter--;
+  var value = this.storage[this.counter];
+  delete this.storage[this.counter];
+  return value;
+}
+
+stackMethods.size = function(){
+  return this.counter < 0 ? 0 : this.counter;
+}
