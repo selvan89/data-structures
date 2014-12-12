79c28c612d70c37707c936d4cf7f8df9bf8136d4
selvan89 <selvanprograms@gmail.com>
2014-12-11 16:40:15 -0800

    Finish linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,40 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
+
   list.addToTail = function(value){
+    var newNode = Node(value);
+    if(list.head === null){
+      list.head = newNode;
+    }
+    if(list.tail === null){
+      list.tail = newNode;
+    }else{
+      list.head.next = newNode;
+      list.tail = newNode;
+    }
+
   };
 
   list.removeHead = function(){
+    var value = list.head.value;
+    list.head = list.head.next;
+    return value;
   };
 
   list.contains = function(target){
+    var recursiveChecker = function(node){
+      if(node.value===target){
+        return true;
+      }
+      else if(node.next === null){
+        return false;
+      }
+      else{
+        return recursiveChecker(node.next);
+      }
+    }
+    return recursiveChecker(list.head);
   };
 
   return list;
