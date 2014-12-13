f8f9c5ff229e0299260359d4672bca71d25b90d9
selvan89 <selvanprograms@gmail.com>
2014-12-12 16:39:53 -0800

    Finish graph, set and tree

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,27 +1,73 @@
+// this.storage = {
+
+//   puppies: {
+//     name: puppies
+//     edges: [kittens, penguins]
+//   }
+// }
 
 
 var Graph = function(){
+  this.storage = {};
+  this.edge = {};
 };
 
 Graph.prototype.addNode = function(node){
+  //check for edges
+
+  this.storage[node] = node;
+  this.edge[node] = [];
+  console.log('this nodes edge: ', this.edge[node])
 };
 
 Graph.prototype.contains = function(node){
+  if(this.storage[node]){
+    return true;
+  } else{
+    return false;
+  }
 };
 
 Graph.prototype.removeNode = function(node){
+  delete this.storage[node];
 };
 
 Graph.prototype.hasEdge = function(fromNode, toNode){
+  // for(var i in this.edge){
+  //   if(this.edge[fromNode]===toNode && this.edge[toNode] === fromNode){
+  //     return true;
+  //   }
+  // }
+  // console.log(this.edge);
+  if(this.storage[fromNode] && this.storage[toNode]){
+    for(var i = 0; i < this.edge[fromNode].length; i++){
+      if (this.edge[fromNode][i] === toNode){
+        return true;
+      }
+      else{
+        return false;
+      }
+    }
+  }
+
+
 };
 
 Graph.prototype.addEdge = function(fromNode, toNode){
+  if((fromNode in this.storage) && (toNode in this.storage)){
+    this.edge[fromNode].push(toNode);
+    this.edge[toNode].push(fromNode);
+  }
 };
 
 Graph.prototype.removeEdge = function(fromNode, toNode){
+
 };
 
 Graph.prototype.forEachNode = function(cb){
+  for(var item in this.storage){
+    cb(this.storage[item]);
+  }
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,23 +3,45 @@ var Tree = function(value){
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
+  newTree.children = [];  // fix me
   return newTree;
 };
-
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+  // if(this.children.length === 0){
+  //   this.children[this.children.length] = Tree(value);
+  // }
+  // console.log('value: ', value);
+  var newerTree = Tree(value);
+  this.children.push(newerTree);
+  // console.log('children: ', this.children[0].value)
 
 };
 
 treeMethods.contains = function(target){
+  var recursiveCheck = function(node){
 
+    if(node.value === target){
+      // console.log('inside the first if: ', node.value, target);
+      // console.log('log true motherfucker: ', node.value===target);
+      return true;
+    }
+
+    // var found = false;
+    for(var i = 0; i<node.children.length; i++){
+      //console.log('current node: ', node.children[i].value, 'target: ', target)
+      if( recursiveCheck(node.children[i]) ){
+        return true;
+      }
+    }
+
+
+    return false;
+  }
+  return recursiveCheck(this);
 };
 
 
